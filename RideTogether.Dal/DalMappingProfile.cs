7c9e1f768752cd25946ceda1cb1631c35fc32ca7
exc3pt1on <shlihanovdev@gmail.com>
2024-04-05 15:27:09 +0300

    Adding CreateMap for UserDao, CredentialsDao and RoleDao.

--- RideTogether.Dal/DalMappingProfile.cs
+++ RideTogether.Dal/DalMappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using RideTogether.Dal.DataObjects.Credentials;
 using RideTogether.Dal.DataObjects.Person;
+using RideTogether.Dal.DataObjects.Role;
 using RideTogether.Dal.DataObjects.Trip;
+using RideTogether.Dal.DataObjects.User;
+using RideTogether.Models.CredentialsModel;
 using RideTogether.Models.PersonModel;
+using RideTogether.Models.RoleModel;
 using RideTogether.Models.TripModel;
+using RideTogether.Models.UserModel;
 
 namespace RideTogether.Dal;
 
@@ -14,6 +20,8 @@ public class DalMappingProfile : Profile
         CreateMap<HitchhikerDao, Hitchhiker>().ReverseMap();
         CreateMap<TripDao, Trip>().ReverseMap();
         
-        // + UserDao, CredentialsDao, RoleDao
+        CreateMap<UserDao, User>().ReverseMap();
+        CreateMap<CredentialsDao, Credentials>().ReverseMap();
+        CreateMap<RoleDao, Role>().ReverseMap();
     }
 }
